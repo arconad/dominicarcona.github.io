ba805656f246dc317133525f77705362dbb5da18
Dominic Arcona <arconad56@gmail.com>
2026-01-06 14:04:51 -0800

    Implement dark mode toggle functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById("dark-toggle");
+
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark");
+}
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+  }
+});
